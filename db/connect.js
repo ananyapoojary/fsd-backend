eff0d43f2c9d4bf8f5189c2683de9169bc9efa43
ananyapoojary <ananyaarunpoojary@gmail.com>
2025-02-20 09:00:48 +0530

    yet another commit

--- db/connect.js
+++ db/connect.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    process.exit(1);  // Exit the process with failure if the DB connection fails
+      // Exit the process with failure if the DB connection fails
   }
 };
 
